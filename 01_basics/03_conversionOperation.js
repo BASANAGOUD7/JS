c6d6b3751f6dcec1e196f35a1f30fc539df32f96
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-09 09:06:58 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+// "33abc" == NaN
+
+//----------------------------------------------------
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1== true
+// ""== false
+// "hitesh" == true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
