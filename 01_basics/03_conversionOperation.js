ef38894754b56675461c5b8431970ce4ba69914b
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-11 09:05:28 +0000

    conversion and comparasion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,22 +1,45 @@
-let score = "33abc"
-console.log(typeof score);
+// let score = "33abc"
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log( valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
 
-// "33abc" == NaN
+// // "33abc" == NaN
 
-//----------------------------------------------------
-let isLoggedIn = 1
+// //----------------------------------------------------
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1== true
-// ""== false
-// "hitesh" == true
+// // 1== true
+// // ""== false
+// // "hitesh" == true
 
-let someNumber = 33;
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+//***************************** Operations *****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let  str1 = "hello"
+// let str2 = "world"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2  + 2);//122
+// console.log(1 + 1 + "2"); //22
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+
+// console.log("02" > 1);//true
+// console.log("2" > 1);//true
+
+// console.log(null > 1);//false
+// console.log(null == 0);//false
+// console.log(null >= 0); //true
+
+// === -> Strict equal
+console.log("2" === 2);//false
