e83673e7c379ee66d447e9c196114bd292cce56d
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-12 01:42:59 +0000

    Datatype Summary

---
+++ dataTypes_summary.js
@@ -0,0 +1,16 @@
+//Primitive DataTypes :- 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+
+//Reference(non primitive):- Arrays, Objects, Functions
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name:"Basanagoud",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
