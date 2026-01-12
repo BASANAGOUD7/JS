0e46332850d151c00e293080bf94398faf6b0132
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-12 02:08:01 +0000

    Stack and Heap Memory

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -3,14 +3,41 @@
 
 
 //Reference(non primitive):- Arrays, Objects, Functions
-const heros = ["shaktiman", "naagraj", "doga"]
-let myObj = {
-    name:"Basanagoud",
-    age: 22
+// const heros = ["shaktiman", "naagraj", "doga"]
+// let myObj = {
+//     name:"Basanagoud",
+//     age: 22
+// }
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof bigNumber);
+
+
+//********************************************************************* */
+
+// Stack Memory(Primitive Tyoe), Heap Memory(Non-Primitive)
+
+let myYtname = "xyz";
+
+let anotherName = myYtname;
+anotherName = "abc";
+console.log(myYtname); //xyz
+console.log(anotherName);//abc
+
+let userOne = {
+    email: "user.gmail.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("hello world");
-}
+let userTwo = userOne;
 
-console.log(typeof bigNumber);
+userTwo.email = "google@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+ 
