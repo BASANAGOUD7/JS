e45bfd9a4f3c15c371a15ffa85d52956796e54ff
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-16 01:50:45 +0000

    Math

--- 01_basics/06_numANDmath.js
+++ 01_basics/06_numANDmath.js
@@ -1,1 +1,21 @@
-const score = 400;
+// const score = 425.23256;
+// console.log(score.toPrecision(5));
+
+// const num = 1000000;
+// console.log(num.toLocaleString('en-IN'));
+
+//--------------------------MATHS--------------------------
+ 
+// console.log(Math);
+// console.log(Math.abs(-4));   
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3, 2, 5, 9));
+
+// console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
