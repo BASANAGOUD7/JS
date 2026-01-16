8e9b1b3e807ab858fb1848f15d1016a1c392a5e5
BASANAGOUD7 <139843143+BASANAGOUD7@users.noreply.github.com>
2026-01-16 01:18:23 +0000

    Strings and numbers

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Bsp";
+const repoCount = 50;
+
+console.log(name + repoCount);
+console.log(`hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gamename = new String('bgmi_');
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf("m"));
+
+
+
